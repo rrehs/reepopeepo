eaa39f552664e95a4bfb9e99a4c491349eef6253
unknown <spinorager338@gmail.com>
2024-11-12 17:23:16 +0800

    new

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',  // Make sure you have an entry file here
+  entry: 'index.html',  // Make sure you have an entry file here
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
